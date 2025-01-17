756918cf4bfb5bd4dbe02e32e3672cbe8be75cf3
mguillen <mguillen@fpj.com.py>
2025-01-17 02:33:51 -0300

    feat: add class for extracting data from json tender template

---
+++ json_main.py
@@ -0,0 +1,12 @@
+from json_extractor import JSONExtractor
+
+def main():
+    json_path = "./inputs/test_json.json"  # Ruta al archivo JSON
+    extractor = JSONExtractor(json_path)
+    
+    extractor.load_json()
+    clauses = extractor.extract_clauses()
+    extractor.save_to_csv(clauses)
+
+if __name__ == "__main__":
+    main()
