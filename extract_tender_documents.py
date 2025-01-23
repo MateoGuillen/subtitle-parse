3a5978f835460a35f3ab6df847b87414120aa9fc
mguillen <mguillen@fpj.com.py>
2025-01-23 15:26:00 -0300

    feat: add data enrichment, records extraction, and join processing - Include `nro_licitacion` directly from input CSV without recalculating. - Add support for enriching licitation data via Contrataciones API. - Extract `records.csv` and join with `ten_documents.csv` for each year. - Perform left join for both PDF and JSON outputs on `compiledRelease/tender/id`. - Ensure additional columns are added dynamically if missing. - Handle HTTP request errors gracefully for individual licitations. - Save enriched and merged data to the specified output CSV files.

--- extract_tender_documents.py
+++ extract_tender_documents.py
@@ -2,21 +2,25 @@ import os
 from utils.ocds_procesor import OCDSProcessor
 
 def main():
-    base_url = "https://www.contrataciones.gov.py/images/opendata-v3/final/ocds"
-    output_path = "./outputs"
-    os.makedirs(output_path, exist_ok=True)
+    base_url = "https://www.contrataciones.gov.py"
+    url_buscador_licitacion = f'{base_url}/buscador/licitaciones.csv?nro_nombre_licitacion='
+    url_ocds_dataset = f'{base_url}/images/opendata-v3/final/ocds'
     
-    years = [2021, 2022, 2023, 2024, 2025]
+    output_path = "./outputs"
+    prefix_name = "merged_tender_data"
+    os.makedirs(output_path, exist_ok=True)
+
+    years = [2025]
     output_pdf = os.path.join(output_path, "ten_documents_pliego_pdf_every_year.csv")
     output_json = os.path.join(output_path, "ten_documents_pliego_json_every_year.csv")
-    
 
-    processor = OCDSProcessor(base_url, output_path)
-    processor.merge_yearly_outputs(years, output_pdf, output_json)
-    
+    processor = OCDSProcessor(url_ocds_dataset, output_path)
 
-    # for year in range(2021, 2026):
-    #     processor.process_year(year)
+    for year in years:
+        processor.process_year(year, prefix_name)
+
+    processor.merge_yearly_outputs(years, output_pdf, output_json, prefix_name )
+    # processor.process_and_enrich_data(output_pdf, output_json, url_buscador_licitacion)  
 
 if __name__ == "__main__":
     main()
--- utils/ocds_procesor.py
+++ utils/ocds_procesor.py
@@ -2,103 +2,267 @@ import os
 import requests
 import zipfile
 import pandas as pd
+from io import StringIO
+
 
 class FileDownloader:
     @staticmethod
     def download_file(url, output_path):
-        response = requests.get(url, stream=True)
-        if response.status_code == 200:
-            with open(output_path, 'wb') as file:
+        print(f"Descargando archivo desde {url}")
+        try:
+            response = requests.get(url, stream=True, timeout=30)
+            response.raise_for_status()  
+            with open(output_path, "wb") as file:
                 for chunk in response.iter_content(chunk_size=1024):
                     file.write(chunk)
-        else:
-            raise Exception(f"Failed to download file from {url}, status code: {response.status_code}")
+            print(f"Archivo descargado y guardado en {output_path}")
+        except requests.exceptions.RequestException as e:
+            print(f"Error al descargar el archivo desde {url}: {e}")
+            raise
 
 class FileExtractor:
     @staticmethod
     def extract_file(zip_path, target_file, output_path):
-        with zipfile.ZipFile(zip_path, 'r') as zip_ref:
-            file_list = zip_ref.namelist()
-            if target_file in file_list:
-                zip_ref.extract(target_file, output_path)
-                return os.path.join(output_path, target_file)
-            else:
-                raise FileNotFoundError(f"{target_file} not found in {zip_path}")
+        print(f"Extrayendo archivo {target_file} de {zip_path}")
+        try:
+            with zipfile.ZipFile(zip_path, "r") as zip_ref:
+                file_list = zip_ref.namelist()
+                if target_file in file_list:
+                    zip_ref.extract(target_file, output_path)
+                    print(f"Archivo extraído a {output_path}")
+                    return os.path.join(output_path, target_file)
+                else:
+                    raise FileNotFoundError(f"{target_file} no encontrado en {zip_path}")
+        except (zipfile.BadZipFile, FileNotFoundError) as e:
+            print(f"Error al extraer el archivo: {e}")
+            raise
+
 
 class CSVProcessor:
     @staticmethod
     def filter_and_process_csv(input_csv, output_csv, column_filters, date_column):
-        df = pd.read_csv(input_csv)
-        
-        # Apply the column filters
-        for column, value in column_filters.items():
-            df = df[df[column] == value]
-        
-        # Keep only the most recent record for each unique tender ID
-        if not df.empty:
-            df[date_column] = pd.to_datetime(df[date_column])
-            df = df.sort_values(by=date_column, ascending=False).drop_duplicates(
-                subset="compiledRelease/tender/id", keep="first"
-            )
-        
-        df.to_csv(output_csv, index=False)
+        try:
+            print(f"Procesando CSV: {input_csv}")
+            df = pd.read_csv(input_csv)
+
+            for column, value in column_filters.items():
+                df = df[df[column] == value]
+
+            if not df.empty:
+                df[date_column] = pd.to_datetime(df[date_column])
+                df = df.sort_values(by=date_column, ascending=False).drop_duplicates(
+                    subset="compiledRelease/tender/id", keep="first"
+                )
+
+            df.to_csv(output_csv, index=False)
+            print(f"CSV procesado guardado en {output_csv}")
+        except Exception as e:
+            print(f"Error al procesar el archivo CSV: {e}")
+            raise
+
 
 class OCDSProcessor:
     def __init__(self, base_url, output_dir):
         self.base_url = base_url
         self.output_dir = output_dir
+        
+    def extract_nro_licitacion(self, open_contracting_id):
+        """Extrae nro_licitacion desde Open Contracting ID."""
+        try:
+            return open_contracting_id.split('-')[2]
+        except IndexError:
+            return None
 
-    def process_year(self, year):
+    def extract_version_pliego(self, url):
+        """Extrae version_pliego desde la URL."""
+        try:
+            return url.split('/')[-1]
+        except IndexError:
+            return None
+
+    def process_year(self, year, prefix_name):
         zip_name = f"masivo_{year}.zip"
         csv_name = f"ten_documents_{year}.csv"
         zip_path = os.path.join(self.output_dir, zip_name)
         csv_path = os.path.join(self.output_dir, csv_name)
-        
-        # Download the ZIP file
-        url = f"{self.base_url}/{year}/masivo.zip"
-        FileDownloader.download_file(url, zip_path)
-        
-        # Extract the target CSV
-        extracted_csv_path = FileExtractor.extract_file(zip_path, "ten_documents.csv", self.output_dir)
-        os.rename(extracted_csv_path, csv_path)
+        record_csv_name = f"record_{year}.csv"
+        record_csv_path = os.path.join(self.output_dir, record_csv_name)
 
-        # Process for PDFs
-        pdf_output = os.path.join(self.output_dir, f"ten_documents_pliego_pdf_{year}.csv")
-        CSVProcessor.filter_and_process_csv(csv_path, pdf_output, {
-            "compiledRelease/tender/documents/0/documentTypeDetails": "Pliego Electrónico de bases y Condiciones",
-            "compiledRelease/tender/documents/0/format": "application/pdf",
-        }, "compiledRelease/tender/documents/0/datePublished")
+        try:
+            # Descargar masivo.zip
+            url = f"{self.base_url}/{year}/masivo.zip"
+            FileDownloader.download_file(url, zip_path)
 
-        # Process for JSONs
-        json_output = os.path.join(self.output_dir, f"ten_documents_pliego_json_{year}.csv")
-        CSVProcessor.filter_and_process_csv(csv_path, json_output, {
-            "compiledRelease/tender/documents/0/documentTypeDetails": "Pliego Electrónico de bases y Condiciones",
-            "compiledRelease/tender/documents/0/format": "application/json",
-        }, "compiledRelease/tender/documents/0/datePublished")
+            # Extraer ten_documents.csv y records.csv
+            extracted_ten_documents_path = FileExtractor.extract_file(zip_path, "ten_documents.csv", self.output_dir)
+            extracted_record_path = FileExtractor.extract_file(zip_path, "records.csv", self.output_dir)
 
-        # Remove ZIP file
-        os.remove(zip_path)
-        
-    def merge_yearly_outputs(self, years, output_pdf, output_json):
-        pdf_frames = []
-        json_frames = []
-        
-        for year in years:
-            pdf_file = os.path.join(self.output_dir, f"ten_documents_pliego_pdf_{year}.csv")
-            json_file = os.path.join(self.output_dir, f"ten_documents_pliego_json_{year}.csv")
+            # Renombrar los archivos extraídos
+            os.rename(extracted_ten_documents_path, csv_path)
+            os.rename(extracted_record_path, record_csv_path)
+
+            # Filtrar datos y generar archivos para PDFs
+            pdf_output = os.path.join(self.output_dir, f"ten_documents_pliego_pdf_{year}.csv")
+            CSVProcessor.filter_and_process_csv(csv_path, pdf_output, {
+                "compiledRelease/tender/documents/0/documentTypeDetails": "Pliego Electrónico de bases y Condiciones",
+                "compiledRelease/tender/documents/0/format": "application/pdf",
+            }, "compiledRelease/tender/documents/0/datePublished")
+
+            # Filtrar datos y generar archivos para JSONs
+            json_output = os.path.join(self.output_dir, f"ten_documents_pliego_json_{year}.csv")
+            CSVProcessor.filter_and_process_csv(csv_path, json_output, {
+                "compiledRelease/tender/documents/0/documentTypeDetails": "Pliego Electrónico de bases y Condiciones",
+                "compiledRelease/tender/documents/0/format": "application/json",
+            }, "compiledRelease/tender/documents/0/datePublished")
+
+            # Realizar un left join entre PDFs y record.csv
+            print(f"Realizando left join para PDFs del año {year}...")
+            pdf_df = pd.read_csv(pdf_output)
+            record_df = pd.read_csv(record_csv_path)
+            pdf_merged_df = pdf_df.merge(record_df, on="compiledRelease/tender/id", how="left")
+
+            # Agregar columnas nro_licitacion y version_pliego
+            pdf_merged_df["nro_licitacion"] = pdf_merged_df["Open Contracting ID"].apply(self.extract_nro_licitacion)
+            pdf_merged_df["version_pliego"] = pdf_merged_df["compiledRelease/tender/documents/0/url"].apply(self.extract_version_pliego)
+
+            pdf_merged_output_path = os.path.join(self.output_dir, f"{prefix_name}_pdf_{year}.csv")
+            pdf_merged_df.to_csv(pdf_merged_output_path, index=False)
+            print(f"Datos combinados para PDFs guardados en {pdf_merged_output_path}")
+
+            # Realizar un left join entre JSONs y record.csv
+            print(f"Realizando left join para JSONs del año {year}...")
+            json_df = pd.read_csv(json_output)
+            json_merged_df = json_df.merge(record_df, on="compiledRelease/tender/id", how="left")
+
+            # Agregar columnas nro_licitacion y version_pliego
+            json_merged_df["nro_licitacion"] = json_merged_df["Open Contracting ID"].apply(self.extract_nro_licitacion)
+            json_merged_df["version_pliego"] = json_merged_df["compiledRelease/tender/documents/0/url"].apply(self.extract_version_pliego)
+
+            json_merged_output_path = os.path.join(self.output_dir, f"{prefix_name}_json_{year}.csv")
+            json_merged_df.to_csv(json_merged_output_path, index=False)
+            print(f"Datos combinados para JSONs guardados en {json_merged_output_path}")
+
+            # Limpiar archivos temporales
+            os.remove(zip_path)
+            print(f"Procesamiento del año {year} completado.")
+            return pdf_merged_output_path, json_merged_output_path
+        except Exception as e:
+            print(f"Error al procesar datos del año {year}: {e}")
+            return None, None
+    def merge_yearly_outputs(self, years, output_pdf, output_json,prefix_name):
+        try:
+            pdf_frames = []
+            json_frames = []
+
+            for year in years:
+                pdf_file = os.path.join(self.output_dir, f"{prefix_name}_pdf_{year}.csv")
+                json_file = os.path.join(self.output_dir, f"{prefix_name}_json_{year}.csv")
+
+                if os.path.exists(pdf_file):
+                    pdf_frames.append(pd.read_csv(pdf_file))
+                if os.path.exists(json_file):
+                    json_frames.append(pd.read_csv(json_file))
+
+            if pdf_frames:
+                pd.concat(pdf_frames).to_csv(output_pdf, index=False)
+
+            if json_frames:
+                pd.concat(json_frames).to_csv(output_json, index=False)
+
+            print(f"Archivos combinados guardados en {output_pdf} y {output_json}")
+        except Exception as e:
+            print(f"Error al combinar archivos: {e}")
+
+    def process_and_enrich_data(self, input_csv, output_csv, base_url):
+        try:
+            df = pd.read_csv(input_csv)
+
+            # Columnas adicionales que queremos incluir
+            additional_columns = [
+                "planificacion_slug", "convocatoria_slug", "adjudicacion_slug",
+                "precalificacion_slug", "convenio_slug","nombre_licitacion", "tipo_procedimiento", "categoria", "convocante",
+                "_etapa_licitacion", "etapa_licitacion", "fecha_entrega_oferta",
+                "tipo_licitacion", "fecha_estimada", "fecha_publicacion_convocatoria", "geo"
+            ]
+            # Asegurar que todas las columnas adicionales estén presentes
+            for col in additional_columns:
+                if col not in df.columns:
+                    df[col] = None
+
+            for idx, row in df.iterrows():
+                nro_licitacion = row.get("nro_licitacion")
+                print(f"Enriqueciendo datos para licitación {nro_licitacion}")
+                if pd.notna(nro_licitacion):
+                    url = f"{base_url}{nro_licitacion}"
+                    try:
+                        response = requests.get(url, timeout=10)
+                        response.raise_for_status()
+                        csv_data = StringIO(response.content.decode("utf-8"))
+                        temp_df = pd.read_csv(csv_data)
+                        if not temp_df.empty:
+                            for col in additional_columns:
+                                if col in temp_df.columns:
+                                    df.at[idx, col] = temp_df.iloc[0].get(col, None)
+                    except requests.exceptions.RequestException as e:
+                        print(f"Error al enriquecer datos para licitación {nro_licitacion}: {e}")
+
+            # Guardar el resultado enriquecido en el archivo de salida
+            df.to_csv(output_csv, index=False)
+            print(f"Datos enriquecidos guardados en {output_csv}")
+        except Exception as e:
+            print(f"Error al enriquecer datos: {e}")
+
+
             
-            if os.path.exists(pdf_file):
-                pdf_frames.append(pd.read_csv(pdf_file))
-            if os.path.exists(json_file):
-                json_frames.append(pd.read_csv(json_file))
-        
-        # Concatenate and save merged PDF files
-        if pdf_frames:
-            pd.concat(pdf_frames).to_csv(output_pdf, index=False)
-        
-        # Concatenate and save merged JSON files
-        if json_frames:
-            pd.concat(json_frames).to_csv(output_json, index=False)
+    def process_ocds_data(self, input_csv, output_csv, api_key):
+        try:
+            print(f"Procesando datos OCDS desde {input_csv}")
+            df = pd.read_csv(input_csv)
 
-   
+            ocds_data = []
+
+            for _, row in df.iterrows():
+                tender_id = row.get("compiledRelease/tender/id")
+                if pd.notna(tender_id):
+                    url = f"https://www.contrataciones.gov.py/datos/api/v3/doc/tender/{tender_id}"
+                    headers = {
+                        "accept": "application/json",
+                        "Authorization": api_key
+                    }
+
+                    try:
+                        response = requests.get(url, headers=headers, timeout=10)
+                        response.raise_for_status()
+                        tender_data = response.json().get("tender", {})
+                        ocds_data.append({
+                            "mainProcurementCategoryDetails": tender_data.get("mainProcurementCategoryDetails"),
+                            "mainProcurementCategory": tender_data.get("mainProcurementCategory"),
+                            "title": tender_data.get("title"),
+                            "procurementMethodDetails": tender_data.get("procurementMethodDetails"),
+                            "procurementMethod": tender_data.get("procurementMethod"),
+                            "id": tender_data.get("id"),
+                            "statusDetails": tender_data.get("statusDetails"),
+                            "awardCriteriaDetails": tender_data.get("awardCriteriaDetails"),
+                        })
+                    except requests.exceptions.RequestException as e:
+                        print(f"Error al obtener datos para ID {tender_id}: {e}")
+                        ocds_data.append({
+                            "mainProcurementCategoryDetails": None,
+                            "mainProcurementCategory": None,
+                            "title": None,
+                            "procurementMethodDetails": None,
+                            "procurementMethod": None,
+                            "id": tender_id,
+                            "statusDetails": None,
+                            "awardCriteriaDetails": None,
+                        })
+
+            # Convertir a DataFrame y combinar con el CSV original
+            ocds_df = pd.DataFrame(ocds_data)
+            enriched_df = pd.concat([df, ocds_df], axis=1)
+
+            enriched_df.to_csv(output_csv, index=False)
+            print(f"Datos procesados y guardados en {output_csv}")
+        except Exception as e:
+            print(f"Error al procesar datos OCDS: {e}")
 
